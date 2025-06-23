32351ba59d7b526e4428a953f5ec6355defa0f79
hgthan <hgthan@gmail.com>
2025-06-23 15:18:25 +0800

    Update duck.py

--- duck.py
+++ duck.py
@@ -6,10 +6,12 @@ temp = 1
 
 while temp < 2:
     response = input()
-    num = random.randint(1,2)
+    num = random.randint(1,3)
     if(response == "goodbye"):
         break
     elif (num == 1):
-        print("quack!\n")
+        print("ooh ooh ah ah!\n")
+    elif (num == 2):
+        print("ooh ooh ahh")
     else:
-        print("quack quack\n")
+        print("ooh ah?\n")
